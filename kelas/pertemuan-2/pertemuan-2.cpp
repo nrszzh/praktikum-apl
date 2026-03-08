0322d567f3ff35183d5c25afd3be7b0664e194d7
Nur Azizah Islamiyah <nrislamiyah14@gmail.com>
2026-03-08 22:42:19 +0800

    p2

---
+++ kelas/pertemuan-2/pertemuan-2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+// int matriks[3][3] = {
+
+// {1, 2, 3}, // Baris ke-0
+// {4, 5, 6}, // Baris ke-1
+// {7, 8, 9} // Baris ke-2
+
+// };
+// // Mengakses elemen
+// cout < matriks[0][1] < endl;
+// cout < matriks[2][0] < endl;
+// // Menampilkan seluruh matriks dengan nested loop
+// for (int i = 0; i < 3; i +) { // Loop baris
+//     for (int j = 0; j < 3; j +) { // Loop kolom
+//         cout < matriks[i][j] < " ";
+//     }
+//     cout < endl;
+// }
+
+
