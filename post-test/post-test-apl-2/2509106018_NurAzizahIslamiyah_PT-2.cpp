8978d690d40c5a6e3f1cd2dedb751f5664384240
Nur Azizah Islamiyah <nrislamiyah14@gmail.com>
2026-03-08 22:50:55 +0800

    declare struct

--- post-test/post-test-apl-2/2509106018_NurAzizahIslamiyah_PT-2.cpp
+++ post-test/post-test-apl-2/2509106018_NurAzizahIslamiyah_PT-2.cpp
@@ -1,1 +1,35 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+#define MAX_SLOT 10
+#define MAX_USER 5
+
+struct waktu {
+    int jam;
+    int menit;
+    string zona_wkt;
+};
+
+struct tiket {
+    string kode_parkir;
+    string jenis_kendaraan;
+    waktu masuk;
+};
+
+struct member {
+    string nama;
+    string password;
+    float saldo;
+    float riwayat = 0;
+};
+
+struct parkir {
+    string plat = "-";
+    int id_pengguna = -1; 
+    bool terisi = false;
+    bool reserved = false;
+    bool maintenance = false;
+    tiket data_tiket;
+};
