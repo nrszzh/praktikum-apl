1cf3fe22bf31b0fb59ecb8ffaeb82b32a916642e
Nur Azizah Islamiyah <nrislamiyah14@gmail.com>
2026-03-10 17:10:54 +0800

    pertemuan 2

--- kelas/pertemuan-2/pertemuan-2.cpp
+++ kelas/pertemuan-2/pertemuan-2.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
 using namespace std;
 
+// int bilangan[5]; // Array integer, 5 elemen
+// string nama[4]; // Array string, 4 elemen
+// float nilai[10]; // Array float, 10 elemen
+// // Deklarasi + Inisialisasi langsung
+// string buah[4] = {"Jeruk", "Mangga", "Nanas", "Apel"};
+// // Deklarasi array tanpa inisialisasi elemen array
+// int angka[3];
+// int angka[0] = 10;
+// int angka[1] = 19;
+// int angka[2] = 30;
+
+// buah[4] = "Rambutan";
+//     for (int i = 0; i < 5; i +) {
+//         cout << buah[i] << endl;
+//     }
+
+//         cout << buah[0] << endl;
+//         cout << angka[2] << endl;
+//         cout << endl;
+//     // For each perulangan
+//     for (string x : buah) {
+//     cout << x << endl;
+//     }
+
+// int angka[5] = {10, 20, 30, 40, 50};
+// // sizeof(angka) = 20 bytes (5 elemen × 4 bytes per int)
+// // sizeof(angka[0]) = 4 bytes (1 elemen int)
+// int panjang = sizeof(angka) / sizeof(angka[0]);
+//     cout << "Panjang array: " << panjang << endl;
+
 // int matriks[3][3] = {
 
 // {1, 2, 3}, // Baris ke-0
@@ -9,14 +39,12 @@ using namespace std;
 
 // };
 // // Mengakses elemen
-// cout < matriks[0][1] < endl;
-// cout < matriks[2][0] < endl;
+// cout << matriks[0][1] << endl;
+// cout << matriks[2][0] << endl;
 // // Menampilkan seluruh matriks dengan nested loop
 // for (int i = 0; i < 3; i +) { // Loop baris
 //     for (int j = 0; j < 3; j +) { // Loop kolom
 //         cout < matriks[i][j] < " ";
 //     }
-//     cout < endl;
+//     cout << endl;
 // }
-
-
