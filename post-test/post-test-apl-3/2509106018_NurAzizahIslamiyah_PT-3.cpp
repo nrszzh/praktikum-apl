4b9f49feaefcb5ed3f0869cf728afd6246b342d0
Nur Azizah Islamiyah <nrislamiyah14@gmail.com>
2026-03-16 23:00:38 +0800

    done member

--- post-test/post-test-apl-3/2509106018_NurAzizahIslamiyah_PT-3.cpp
+++ post-test/post-test-apl-3/2509106018_NurAzizahIslamiyah_PT-3.cpp
@@ -272,3 +272,9 @@ void menu_member(int id, data_member list[], data_parkir denah[2][20]) {
             header_pendek("TOP UP SALDO");
             top_up_saldo(list[id]);
             kembali();
+
+        } else if (pilihan == 4) 
+        read_user(denah);
+
+    } while (pilihan != 0);
+}
