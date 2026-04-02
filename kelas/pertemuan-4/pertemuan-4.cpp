b0205ae5dbae2d38bc8923a85af96dd97cac6f34
Nur Azizah Islamiyah <nrislamiyah14@gmail.com>
2026-04-02 20:47:02 +0800

    ptr

--- kelas/pertemuan-4/pertemuan-4.cpp
+++ kelas/pertemuan-4/pertemuan-4.cpp
@@ -2,3 +2,17 @@
 #include <string>
 using namespace std;
 
+int main(){
+    string rumahUcup = "Ucup";
+
+    cout << rumahUcup << endl;
+    cout << &rumahUcup << endl;
+
+    return 0;
+}
+
+struct Address{
+    string kota;
+    string provinsi;
+    string negara;
+};
