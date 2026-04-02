b636717f5ae6733195ee6d9a5a01151d1f712ad6
Nur Azizah Islamiyah <nrislamiyah14@gmail.com>
2026-04-02 20:52:25 +0800

    *&

--- kelas/pertemuan-4/pertemuan-4.cpp
+++ kelas/pertemuan-4/pertemuan-4.cpp
@@ -11,22 +11,38 @@ using namespace std;
 //     return 0;
 // }
 
-struct Address{
-    string kota;
-    string provinsi;
-    string negara;
-};
+// struct Address{
+//     string kota;
+//     string provinsi;
+//     string negara;
+// };
+
+// int main(){
+//     Address address1, address2;
+//     address1.kota = "Samarinda";
+//     address1.provinsi = "Kalimantan Timur";
+//     address1.negara = "Indonesia";
+
+//     address2 = address1;
+//     address2.kota = "IKN";
+
+//     cout << address1.kota << endl;
+//     cout << address2.kota << endl;
+//     return 0;
+// }
 
 int main(){
-    Address address1, address2;
-    address1.kota = "Samarinda";
-    address1.provinsi = "Kalimantan Timur";
-    address1.negara = "Indonesia";
+    string var = "Aku Variabel";
+    string* varPtr = &var;
 
-    address2 = address1;
-    address2.kota = "IKN";
-
-    cout << address1.kota << endl;
-    cout << address2.kota << endl;
+    cout << "Hasil dari varPtr (Alamat): " << varPtr << endl;
+    cout << "Hasil dari *varPtr (Nilai): " << *varPtr << endl;
+    cout << endl << "Kesimpulannya varPtr isi nya alamatnya var" << endl;
+    cout << "*varPtr hasilnya value dari var" << endl;
+    cout << endl;
+    cout << "Hasil/value dari var: " << var << endl;
+    cout << "Hasil/value dari alamat var (&var): " << &var << endl;
+    cout << "Hasil/value dari varPtr: " << varPtr << endl;
+    cout << "Hasil/value dari *varPtr: " << *varPtr << endl;
     return 0;
 }
