fdd99d95d02f2d896c5d8bd3648b4e3db2ee491b
Nur Azizah Islamiyah <nrislamiyah14@gmail.com>
2026-04-02 20:54:22 +0800

    pertemuan 4

--- kelas/pertemuan-4/pertemuan-4.cpp
+++ kelas/pertemuan-4/pertemuan-4.cpp
@@ -31,18 +31,53 @@ using namespace std;
 //     return 0;
 // }
 
-int main(){
-    string var = "Aku Variabel";
-    string* varPtr = &var;
+// int main(){
+//     string var = "Aku Variabel";
+//     string* varPtr = &var;
 
-    cout << "Hasil dari varPtr (Alamat): " << varPtr << endl;
-    cout << "Hasil dari *varPtr (Nilai): " << *varPtr << endl;
-    cout << endl << "Kesimpulannya varPtr isi nya alamatnya var" << endl;
-    cout << "*varPtr hasilnya value dari var" << endl;
-    cout << endl;
-    cout << "Hasil/value dari var: " << var << endl;
-    cout << "Hasil/value dari alamat var (&var): " << &var << endl;
-    cout << "Hasil/value dari varPtr: " << varPtr << endl;
-    cout << "Hasil/value dari *varPtr: " << *varPtr << endl;
-    return 0;
-}
+//     cout << "Hasil dari varPtr (Alamat): " << varPtr << endl;
+//     cout << "Hasil dari *varPtr (Nilai): " << *varPtr << endl;
+//     cout << endl << "Kesimpulannya varPtr isi nya alamatnya var" << endl;
+//     cout << "*varPtr hasilnya value dari var" << endl;
+//     cout << endl;
+//     cout << "Hasil/value dari var: " << var << endl;
+//     cout << "Hasil/value dari alamat var (&var): " << &var << endl;
+//     cout << "Hasil/value dari varPtr: " << varPtr << endl;
+//     cout << "Hasil/value dari *varPtr: " << *varPtr << endl;
+//     return 0;
+// }
+
+// int main(){
+//     cout << "Pointer yang menunjuk ke suatu array" << endl;
+//     int a[5] = {1, 2, 3, 4, 5};
+//     int (*aPtr)[5] = &a;
+//     for(int i = 0; i < 5; i++){
+//         cout << *aPtr << endl;
+//     }
+
+//     cout << "\nPointer yang menunjuk ke arah elemen array" << endl;
+//     int b[5] = {1, 2, 3, 4, 5};
+//     int *bPtr = b;
+//     for(int i = 0; i < 5; i++){
+//         cout << "Alamat: " << bPtr << " | Nilai: " << *bPtr << endl;
+//         bPtr++;
+//     }
+
+// }
+
+
+// // Parameter 'a' adalah pointer (*)
+// int ubahNilai(int *a, int b){
+//     return *a = b;
+// }
+
+// int main () {
+//     int a, b;
+//     a = 5;
+//     b = 20;
+
+//     ubahNilai(&a, b);
+
+//     cout << "Nilai a setelah diubah (Pointer): " << a << endl;
+//     return 0;
+// }
