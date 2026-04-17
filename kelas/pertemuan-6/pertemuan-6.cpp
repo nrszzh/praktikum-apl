5271eb3e0fa058528657a33f41bf1d0137af6eef
Nur Azizah Islamiyah <nrislamiyah14@gmail.com>
2026-04-18 07:48:06 +0800

    linear

--- kelas/pertemuan-6/pertemuan-6.cpp
+++ kelas/pertemuan-6/pertemuan-6.cpp
@@ -42,3 +42,29 @@ using namespace std;
 
 //     return 0;
 // }
+
+int main(){
+    int data[] = {12, 45, 7, 23, 56, 10, 34};
+    int n = sizeof(data) / sizeof(data[0]); // Menghitung jumlah elemen
+
+    int cari;
+    bool found = false;
+
+    cout << "Masukkan angka yang ingin dicari: ";
+    cin >> cari;
+
+    // Proses Linear Search
+    for(int i = 0; i < n; i++){
+        if(data[i] == cari){
+            cout << "Data ditemukan pada indeks ke-" << i << endl;
+            found = true;
+            break; // Berhenti jika sudah ketemu
+        }
+    }
+
+    if(!found){
+        cout << "Data tidak ditemukan dalam array." << endl;
+    }
+
+    return 0;
+}
